5ced56762986dc7a72c4a86ad781c53bcc0053cb
Victor Lino <victor.goncallino@gmail.com>
2023-10-07 17:22:05 -0300

    teste

--- apps/rh-controle-web/src/app/app.routes.ts
+++ apps/rh-controle-web/src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    // {
-    //   path: '',
-    //   loadChildren: () => import('./public/public.module').then(x => x.PublicModule)
-    // },
-    {
-      path: 'authenticated',
-      loadChildren: () => import('./authenticated/authenticated.module').then(x => x.AuthenticatedModule),
-      // canActivate: [AdminGuard]
-    },
-  ];
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./public/public.module').then(x => x.PublicModule)
+  // },
+  {
+    path: 'authenticated',
+    loadChildren: () => import('./authenticated/authenticated.module').then(x => x.AuthenticatedModule),
+    // canActivate: [AdminGuard]
+  },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
